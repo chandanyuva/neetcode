fe133cad1e015311096188fb8e6f8aa2425db8ad
arch_laptop <chandanyuva1511@gmail.com>
2026-01-20 20:06:57 +0530

    1. Contains Duplicate

--- contains_duplicate.js
+++ contains_duplicate.js
@@ -1,1 +1,15 @@
-console.log("git_init")
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+// Example 1:
+// Input: nums = [1, 2, 3, 3]
+// Output: true
+// Example 2:
+// Input: nums = [1, 2, 3, 4]
+// Output: false
+
+nums = [1, 2, 3, 3];
+
+function hasDuplicate(nums) {
+  return new Set(nums).size !== nums.length;
+}
+
+console.log(hasDuplicate(nums));
